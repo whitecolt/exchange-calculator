f30d12897cc53fafe2ad2bf9767dbcc5b08e1001
whitecolt <whitecolt8@gmail.com>
2020-03-19 21:11:22 +0300

    created first version of exchange calculatror using JSON

---
+++ script.js
@@ -0,0 +1,111 @@
+let inputRub = document.getElementById('rub'),
+  inputUsd = document.getElementById('usd'),
+  btnClear = document.querySelector('button');
+
+inputRub.addEventListener('input', () => {
+  let request = new XMLHttpRequest();
+
+  request.open('GET', './current.json');
+  request.setRequestHeader('Content-type', 'application.json; charset=utf-8');
+  request.send();
+
+  request.addEventListener('readystatechange', () => {
+    if (request.readyState === 4 && request.status == 200) {
+      let data = JSON.parse(request.response);
+
+      inputUsd.value = (inputRub.value / data.usd).toFixed(2);
+
+    } else {
+      inputUsd.value = 'шото пошло не так, сорямба';
+    }
+  });
+});
+
+inputUsd.addEventListener('input', () => {
+  let request = new XMLHttpRequest();
+
+  request.open('GET', './current.json');
+  request.setRequestHeader('Content-type', 'application.json; charset=utf-8');
+  request.send();
+
+  request.addEventListener('readystatechange', () => {
+    if (request.readyState === 4 && request.status == 200) {
+      let data = JSON.parse(request.response);
+
+      inputRub.value = (inputUsd.value * data.usd).toFixed(0);
+    } else {
+      inputRub.value = "шото таки снова пошло не так, Абрам!";
+    }
+  });
+});
+
+btnClear.addEventListener('click', () => {
+  inputRub.value = '';
+  inputUsd.value = '';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let inputRub = document.getElementById('rub'),
+//     inputUsd = document.getElementById('usd');
+
+// inputRub.addEventListener('input', () => {
+
+//     function catchData() {
+
+//         return new Promise(function(resolve, reject){
+//             let request = new XMLHttpRequest();
+//             request.open("GET", "js/current.json");
+
+//             request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+//             request.send();
+
+//             request.onload = function() {
+//                 if(request.readyState === 4) {
+//                         if(request.status == 200) {
+//                             resolve(this.response)
+//                         }
+//                         else {
+//                             reject();
+
+//                         }
+//                 }
+//             }
+//         });
+//     };
+
+//     catchData()
+//     .then(response => {
+//         console.log(response);
+//         let data = JSON.parse(response);
+//         inputUsd.value = inputRub.value / data.usd;
+//     })
+//     .then(() => console.log(5000))
+//     .catch(() => inputUsd.value = "Р§С‚Рѕ-С‚Рѕ РїРѕС€Р»Рѕ РЅРµ С‚Р°Рє")
+
+
+// });
